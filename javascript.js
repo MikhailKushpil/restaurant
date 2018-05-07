ddb62e20caddca2e06dbc0bfd54f7a9b8468f28b
mikhail kushpil <misha-ua@i.ua>
2018-05-07 13:14:39 +0300

    add function toggle, delet code, rewrite class HTML

--- javascript.js
+++ javascript.js
@@ -29,188 +29,42 @@ $(document).ready(function(){ // виполним функцию как толь
         prevImage.fadeIn(1000);
         prevImage.addClass("curry");
     });
-
-    $(function () {
-        $("#first_food_1").click(function () {
-            $("#first_food_1_img").toggle(500);
-        });
-    });
-    $(function () {
-        $("#first_food_2").click(function () {
-            $("#first_food_2_img").toggle(500);
-        });
-    });
-    $(function () {
-            $("#Lunch_0").click(function () {
-                $("#Lunch_0_img").slideToggle(250);
-            });
-    });
-    $(function () {
-            $("#Lunch_1").click(function () {
-                $("#Lunch_1_img").slideToggle(250);
-            });
-    });
-    $(function () {
-            $("#Lunch_2").click(function () {
-                $("#Lunch_2_img").slideToggle(250);
-            });
-    });
-    $(function () {
-        var button_first = $("#button_first");
-        var footer_anim_1 = $("#footer_anim_1");
-        var footer_anim_1_style = {fontSize: "100px"};
-        footer_anim_1.click(function (e) {
-            footer_anim_1.animate(footer_anim_1_style, 400);
-        });
-    });
-    $(function () {
-        var button_first = $("#button_first");
-        var footer_anim_2 = $("#footer_anim_2");
-        var footer_anim_2_style = {fontSize: "120px"};
-        footer_anim_2.click(function (e) {
-            footer_anim_2.animate(footer_anim_2_style, 400);
-        });
-    });
-    $(function () {
-        var button_first = $("#button_first");
-        var footer_anim_3 = $("#footer_anim_3");
-        var footer_anim_3_style = {fontSize: "110px"};
-        footer_anim_3.click(function (e) {
-            footer_anim_3.animate(footer_anim_3_style, 400);
-            footer_anim_3.style.color ;
-        });
-    });
-    $(function () {
-        var button_first = $("#button_first");
-        var footer_anim_4 = $("#footer_anim_4");
-        var footer_anim_4_style = {fontSize: "100px"};
-        footer_anim_4.click(function (e) {
-            footer_anim_4.animate(footer_anim_4_style, 400);
-            footer_anim_4.style.color ;
-        });
-    });
-    $(function () {
-        var button_first = $("#button_first");
-        var footer_anim_5 = $("#footer_anim_5");
-        var footer_anim_5_style = {fontSize: "120px"};
-        footer_anim_5.click(function (e) {
-            footer_anim_5.animate(footer_anim_5_style, 400);
-            footer_anim_5.style.color ;
-        });
-    });
-    var click_Main_Courses_0 = false;
-    var click_Main_Courses_1 = false;
-    var click_Main_Courses_2 = false;
-    var click_Main_Courses_3 = false;
-
-    var Main_Courses_0 = document.getElementById("Main_Courses_0");
-    var Main_Courses_1 = document.getElementById("Main_Courses_1");
-    var Main_Courses_2 = document.getElementById("Main_Courses_2");
-    var Main_Courses_3 = document.getElementById("Main_Courses_3");
-
-    Main_Courses_0.addEventListener("click" , function () {
-        var foodFoto = document.getElementsByClassName("foodFoto");
-        if ( click_Main_Courses_0 != true){
-            foodFoto[5].style.display = 'block';
-            click_Main_Courses_0 = true;
+    (function(){
+        function toggleImg(){
+            let li = document.querySelectorAll("li");
+            let foodPhoto = document.querySelectorAll(".foodPhoto");
+            for(let i=0; li.length > i; i++){
+                li[i].addEventListener("click", function(){
+                    if(foodPhoto[i].classList.contains("showImg")){
+                        foodPhoto[i].classList.remove("showImg");
+                    } else{
+                        foodPhoto[i].classList.add("showImg");
+                    }
+                } );
+            }
         }
-        else {
-            foodFoto[5].style.display = "none";
-            click_Main_Courses_0 = false;
-        }
-    }, false);
-    Main_Courses_1.addEventListener("click" , function () {
-        var foodFoto = document.getElementsByClassName("foodFoto");
-        if ( click_Main_Courses_1 != true){
-            foodFoto[6].style.display = 'block';
-            click_Main_Courses_1 = true;
-        }
-        else {
-            foodFoto[6].style.display = "none";
-            click_Main_Courses_1 = false;
-        }
-    }, false);
-    Main_Courses_2.addEventListener("click" , function () {
-        var foodFoto = document.getElementsByClassName("foodFoto");
-        if ( click_Main_Courses_2 != true){
-            foodFoto[7].style.display = 'block';
-            click_Main_Courses_2 = true;
-        }
-        else {
-            foodFoto[7].style.display = "none";
-            click_Main_Courses_2 = false;
-        }
-    }, false);
-    Main_Courses_3.addEventListener("click" , function () {
-        var foodFoto = document.getElementsByClassName("foodFoto");
-        if ( click_Main_Courses_3 != true){
-            foodFoto[8].style.display = 'block';
-            click_Main_Courses_3 = true;
-        }
-        else {
-            foodFoto[8].style.display = "none";
-            click_Main_Courses_3 = false;
-        }
-    }, false);
-
-    var click_Dessert_0 = false;
-    var click_Dessert_1 = false;
-    var Dessert_0 = document.getElementById("Dessert_0");
-    var Dessert_1 = document.getElementById("Dessert_1");
-
-
-    Dessert_0.addEventListener("click" , function () {
-        var foodFoto = document.getElementsByClassName("foodFoto");
-        if ( click_Dessert_0 != true){
-            foodFoto[9].style.display = 'block';
-            click_Dessert_0 = true;
-        }
-        else {
-            foodFoto[9].style.display = "none";
-            click_Dessert_0 = false;
-        }
-    }, false);
-
-    Dessert_1.addEventListener("click" , function () {
-        var foodFoto = document.getElementsByClassName("foodFoto");
-        if ( click_Dessert_1 != true){
-            foodFoto[10].style.display = 'block';
-            click_Dessert_1 = true;
-        }
-        else {
-            foodFoto[10].style.display = "none";
-            click_Dessert_1 = false;
-        }
-    }, false);
-
-    var click_Drinks_0 = false;
-    var click_Drinks_1 = false;
-
-    var Drinks_0 = document.getElementById("Drinks_0");
-    var Drinks_1 = document.getElementById("Drinks_1");
-
-
-    Drinks_0.addEventListener("click" , function () {
-        var foodFoto = document.getElementsByClassName("foodFoto");
-        if ( click_Drinks_0 != true){
-            foodFoto[11].style.display = 'block';
-            click_Drinks_0 = true;
-        }
-        else {
-            foodFoto[11].style.display = "none";
-            click_Drinks_0 = false;
-        }
-    }, false);
-
-    Drinks_1.addEventListener("click" , function () {
-        var foodFoto = document.getElementsByClassName("foodFoto");
-        if ( click_Drinks_1 != true){
-            foodFoto[12].style.display = 'block';
-            click_Drinks_1 = true;
-        }
-        else {
-            foodFoto[12].style.display = "none";
-            click_Drinks_1 = false;
-        }
-    }, false);
+        toggleImg();
+    }());
+    document.getElementById("defaultOpen").click();
 });
+
+function openMenu(menuName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        // Remove the background color of all tablinks/buttons
+        tablinks = document.getElementsByClassName("tablink");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].style.backgroundColor = "";
+        }
+
+        // Show the specific tab content
+        document.getElementById(menuName).style.display = "block";
+
+        // Add the specific color to the button used to open the tab content
+        elmnt.style.backgroundColor = color;
+}
